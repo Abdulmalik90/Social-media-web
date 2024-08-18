c888c09eaf815cf54224f631b2f84be5ee1d103c
Abdulmalik Alkhalifh <bdalmlk832@gmail.com>
2024-08-18 21:41:12 +0300

    check point for losing internet

--- home page/main.js
+++ home page/main.js
@@ -1,15 +1,13 @@
 
 // get posts
 function getPosts(){
-    axios.get("https://tarmeezacademy.com/api/v1/posts?limit=50")
+    axios.get("https://tarmeezacademy.com/api/v1/posts?limit=150")
     .then((response)=>{
         let posts = response.data.data
         
-        console.log(posts)
         let postsDiv = document.getElementById("posts")
         postsDiv.innerHTML = ""
         for (post of posts){
-            let tags = post.tags
             if (String(post.title) == "null"){
                 post.title = ""
             }
@@ -41,16 +39,30 @@ function getPosts(){
                     <p class="card-text">${post.body}</p>
                     <hr>
                 </div>
-                <div class="container mb-3" >
-                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-chat" viewBox="0 0 16 16" style="cursor: pointer; margin-right:10px">
-                        <path d="M2.678 11.894a1 1 0 0 1 .287.801 11 11 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8 8 0 0 0 8 14c3.996 0 7-2.807 7-6s-3.004-6-7-6-7 2.808-7 6c0 1.468.617 2.83 1.678 3.894m-.493 3.905a22 22 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a10 10 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9 9 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105"/>
-                    </svg>
-                    <h5 style="font-weight: 100; display: inline;">(${post.comments_count}) Comments</h5>
-                    <div id="tags-div" style="display:inline; width:100%"></div>
+                <div class="container mb-3" style="display: flex; justify-content: space-between;">
+                    <div style="margin-left: 5px; margin-right: 5px;">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-chat" viewBox="0 0 16 16" style="cursor: pointer; margin-right:10px">
+                            <path d="M2.678 11.894a1 1 0 0 1 .287.801 11 11 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8 8 0 0 0 8 14c3.996 0 7-2.807 7-6s-3.004-6-7-6-7 2.808-7 6c0 1.468.617 2.83 1.678 3.894m-.493 3.905a22 22 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a10 10 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9 9 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105"/>
+                        </svg>
+                        
+                        <h5 style="font-weight: 100; display: inline;">(${post.comments_count}) Comments</h5>
+                    </div>
+                    
+                    <div id="post-tags-div${post.id}">
+                        <button style="background-color: gray; color: white; border: none; border-radius: 10px">hello world </button>
+                    </div>
                 </div>
             </div>
             `
-            
+            let post_tags_div = document.getElementById(`post-tags-div${post.id}`).innerHTML = ``
+            post_tags_div.innerHTML = ``
+
+            let tags = post.tags 
+            for (tag of tags){
+                post_tags_div.innerHTML += `
+                <button style="background-color: gray; color: white; border: none; border-radius: 10px">${tag.name}</button>
+                `
+            }
         }
     }).catch((error) =>{
         let errorPosts = document.getElementById("error-get-post")
@@ -104,7 +116,7 @@ logoutBtn.addEventListener("click", ()=>{
         let alertSuccess = document.getElementById("alert-sign-log")
         alertSuccess.classList.remove("alert-success")
         alertSuccess.classList.add("alert-danger")
-        alertSuccess.innerHTML = "You have successfully logged out!!"
+        alertSuccess.innerHTML = `You have successfully logged out!!`
         alertSuccess.style.display = "block"
         setTimeout(()=>{
             alertSuccess.style.display = "none"
@@ -141,7 +153,7 @@ function signIn(email, username, password, name){
         let alertSuccess = document.getElementById("alert-sign-log")
         alertSuccess.classList.add("alert-success")
         alertSuccess.classList.remove("alert-danger")
-        alertSuccess.innerHTML = "Registration completed successfully!!"
+        alertSuccess.innerHTML = `Registration completed successfully!!`
         alertSuccess.style.display = "block"
         setTimeout(()=>{
             alertSuccess.style.display = "none"
@@ -182,7 +194,8 @@ function logIn(username, password){
         let alertSuccess = document.getElementById("alert-sign-log")
         alertSuccess.classList.add("alert-success")
         alertSuccess.classList.remove("alert-danger")
-        alertSuccess.innerHTML = "You have been logged in successfully!!"
+        // todo : add close button of the alert
+        alertSuccess.innerHTML = `You have been logged in successfully!!`
         alertSuccess.style.display = "block"
         setTimeout(()=>{
             alertSuccess.style.display = "none"
